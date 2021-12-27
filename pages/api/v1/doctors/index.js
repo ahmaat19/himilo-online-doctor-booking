5e00b8f44d123c2a42b41dc45434cacb8d278047
Ahmed <ahmaat19@gmail.com>
2021-12-27 11:51:30 +0300

    adding doctor validation

--- pages/api/v1/doctors/index.js
+++ pages/api/v1/doctors/index.js
@@ -1,5 +1,6 @@
 import nc from 'next-connect'
 import { PrismaClient } from '@prisma/client'
+import moment from 'moment'
 
 const prisma = new PrismaClient()
 
@@ -7,8 +8,12 @@ const handler = nc()
 
 export default handler.get(async (req, res) => {
   try {
-    const doctors = await prisma.doctors.findMany()
-    return res.status(200).json(doctors)
+    const doctors = await prisma.doctors.findMany({where: {
+      Doctor: 'Yes',
+      Active: 'Yes',
+      WorkingDays: {contains: moment().format('dddd'),}
+    }})
+    return res.status(200).json({total: doctors.length, doctors})
   } catch (error) {
     throw new Error(error)
   }
